47dc0126dde3626b147582f7f72495b6f200bbd9
sevelater <selmeczi.vilmos.vazul@gmail.com>
2025-03-26 17:43:02 +0100

    metadata fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,23 @@ import Footer from "./components/connect-us/page"
 import Navbar from "./components/navbar/page"
 
 export const metadata: Metadata = {
-  title: "Márkus Szalon",
-  description: "Fodrászat",
+  title: 'Márkus Szalon',
+  description: 'Barátságos fodrászat Budakeszin. Hajgyógyászat specialista. Egészséges, szép hajért fordulj hozzám!',
+  openGraph: {
+    title: 'Márkus Szalon',
+    description: 'Barátságos fodrász Budakeszin. Hajgyógyászat specialista. Egészséges, szép hajért fordulj hozzám!',
+    siteName: 'Márkus Szalon',
+    images: [
+      {
+        url: 'https://next-app-sevelaters-projects.vercel.app/logo.png',
+        width: 600,
+        height: 600,
+        alt: 'Márkus Szalon logó',
+      },
+    ],
+    locale: 'hu_HU',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({
